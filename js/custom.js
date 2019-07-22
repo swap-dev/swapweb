537996ac4f0853ea339a5e16279180bf1877312e
Supercircles <LeavesLoves@gmail.com>
2019-07-21 20:20:52 -0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -191,10 +191,6 @@ request2.onerror = () => {
 }
 
 // Google Translate
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
-}
-
 function triggerHtmlEvent(element, eventName) {
   var event;
   if (document.createEvent) {
